419150894c5c0f750a371b7e161a12f08e8a2a94
Ярослав Латин <yaroslav.khaustovich@yandex.ru>
2022-03-04 00:37:45 +0300

    вроде рабочая версия с методички

--- laba1/screen.h
+++ laba1/screen.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<iostream>
 const int XMAX = 120;
 const int YMAX = 50;
 class point
@@ -8,11 +9,13 @@ public:
 	point(int a = 0, int b = 0) :x(a), y(b) {}
 };
 
-void put_point(int a, int b);
-void put_point(point p) { put_point(p.x, p.y); }
-void put_line(int, int, int, int);
-void put_line(point a, point b) { put_line(a.x, a.y, b.x, b.y); }
-void screen_init();
-void screen_destroy();
-void screen_refresh();
-void screen_clear();
+
+void put_point(int a, int b);//вывод точки
+void put_point(point p);
+void put_line(int x0, int y0, int x1, int y1);//вывод линии
+void put_line(point a, point b);
+void screen_init();//создание экрана
+void screen_destroy();//удаление
+void screen_refresh();//обновление
+void screen_clear();//очистка
+bool on_screen(int a, int b);//проверка попадания точки на экране
